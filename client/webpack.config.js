bbfc3235f56f800b68a466964e1ec9d38d9bd2f0
JD Tadlock <jt7903@gmail.com>
2023-05-02 13:06:15 -0400

    converted clientside code to client folder

---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Basics Example',
+      inject: 'body',
+      template: './src/public/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
+      }
+    ],
+  }
+}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('./css/style.css');
-require('./css/another.css');
-require('./css/example.scss');
-
-const doSomething = require('./lib/helpers');
-
-doSomething();
